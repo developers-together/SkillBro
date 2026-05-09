7f2a69b3e6f46f2497807995bb936373b95e27bf
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-05-09 07:18:50 +0300

    Update utils library

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,12 +1,11 @@
-import type { InertiaLinkProps } from '@inertiajs/vue3';
 import { clsx } from 'clsx';
-import type { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import type { ClassValue } from 'clsx';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export function toUrl(href: NonNullable<InertiaLinkProps['href']>) {
+export function toUrl(href: string | { url: string }) {
     return typeof href === 'string' ? href : href?.url;
 }
