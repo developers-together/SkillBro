c9f6f0a6392cc3fda298023d8a3b9b36c75d3f46
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-05-09 07:19:42 +0300

    Add new skillbro App.vue component

---
+++ resources/js/skillbro/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { RouterView } from 'vue-router';
+import { Toaster } from '@/components/ui/sonner';
+import { useAuthSession } from './composables/useAuthSession';
+
+const { bootstrapSession } = useAuthSession();
+
+onMounted(async () => {
+    await bootstrapSession();
+});
+</script>
+
+<template>
+    <RouterView />
+    <Toaster />
+</template>
+
